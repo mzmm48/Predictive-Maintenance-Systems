d368e165039dab9e252e099f785f63e58a221759
Moritz Zimmer <moritz.zimmer@mnd.thm.de>
2026-02-05 18:49:31 +0100

    entfernte datein

--- src/deprecated/Main.py
+++ src/deprecated/Main.py
@@ -12,8 +12,8 @@ from predict import do_prediction
 
 berlin_tz = ZoneInfo("Europe/Berlin")
 
-x_test = joblib.load('../data/X_test.joblib')
-y_test = joblib.load('../data/Y_test.joblib')
+x_test = joblib.load('../../data/X_test.joblib')
+y_test = joblib.load('../../data/Y_test.joblib')
 
 x_test2 = x_test.reset_index(drop=True)
 y_test2 = y_test.reset_index(drop=True)
@@ -34,65 +34,6 @@ def simulate_time_stream(x_test2, y_test2, delay_seconds=0.01, model_name: str =
         do_prediction(x_test, y_test, model_name= model_name)
 
         time.sleep(delay_seconds)
-
-#Wertet das Modell aus
-def evaluate_model_metrics(model_name: str = "Random_Forest"):
-
-    # Testdaten laden
-    X_test = joblib.load("../data/X_test.joblib")
-    y_test = joblib.load("../data/Y_test.joblib")  # achte auf Dateinamen-Konsistenz!
-
-    with open(f"../data/models/{model_name}.pkl", "rb") as f:
-        model_dict = pickle.load(f)
-
-    model = model_dict["model"]
-
-    # Vorhersagen
-    y_pred = model.predict(X_test)
-
-    # Probabilities (falls vorhanden)
-    y_prob = None
-    if hasattr(model, "predict_proba"):
-        y_prob = model.predict_proba(X_test)[:, 1]
-    elif hasattr(model, "decision_function"):
-        scores = model.decision_function(X_test)
-        y_prob = 1 / (1 + np.exp(-scores))
-
-    # Grundlegende Kennzahlen
-    acc = accuracy_score(y_test, y_pred)
-    precision, recall, f1, _ = precision_recall_fscore_support(
-        y_test, y_pred, average="binary", zero_division=0
-    )
-
-    # Confusion Matrix
-    cm = confusion_matrix(y_test, y_pred).tolist()  # als Liste für JSON
-
-    # ROC-AUC falls Probabilities da
-    auc = None
-    if y_prob is not None:
-        try:
-            auc = roc_auc_score(y_test, y_prob)
-        except ValueError:
-            auc = None
-
-    # Classification Report als Text
-    report_text = classification_report(y_test, y_pred)
-
-    return {
-        "model_name": model_name,
-        "metrics": {
-            "accuracy": acc,
-            "precision": precision,
-            "recall": recall,
-            "f1": f1,
-            "roc_auc": auc,
-        },
-        "confusion_matrix": {
-            "labels": ["Kein Ausfall", "Ausfall"],
-            "matrix": cm,
-        },
-        "classification_report": report_text,
-    }
 
 
 def simulate_time_stream_collect(x_test2, y_test2, model_name: str = 'Random_Forest', limit: int | None = None):
